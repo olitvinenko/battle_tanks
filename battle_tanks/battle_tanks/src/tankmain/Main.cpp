e56a6827e43a8a943d5195d0f443170054b85dd1
olitvinenko <alex.litvinenko.gm@gmail.com>
2016-08-09 22:52:54 +0300

    external libs added
    lua, oggvorbis, pluto, utf stuff, glfw, zlib

---
+++ battle_tanks/battle_tanks/src/tankmain/Main.cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <windef.h>
+int APIENTRY WinMain( HINSTANCE, // hInstance
+                      HINSTANCE, // hPrevInstance
+                      LPSTR, // lpCmdLine
+                      int) // nCmdShow
+#else
+int main(int, const char**)
+#endif
+try
+{
+	srand((unsigned int) time(nullptr));
+
+#if defined(_DEBUG) && defined(_WIN32) // memory leaks detection
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+	
+
+	return 0;
+}
+catch (const std::exception &e)
+{
+	return 1;
+}
+
+
