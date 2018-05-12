4527ed4cb7239074753484ee9bf8e4d5fb4b9e79
Oleksiy Lytvynenko <o.lytvynenko@plarium.com>
2018-05-12 13:01:48 +0300

    rooback to the vs2015

--- battle_tanks/src/tankmain/Main.cpp
+++ battle_tanks/src/tankmain/Main.cpp
@@ -2,10 +2,36 @@
 #include <ctime>
 #include <algorithm>
 
+
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <windef.h>
+
+#include <string>
+#include <stdio.h>  /* defines FILENAME_MAX */
+#ifdef _WIN32
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+
+std::string CurrentWorkingDirectory()
+{
+	char cCurrentPath[FILENAME_MAX];
+
+	if (!GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
+	{
+		return std::string();
+	}
+
+	cCurrentPath[sizeof(cCurrentPath) - 1] = '\0'; /* not really required */
+	return std::string(cCurrentPath);
+}
+
+
 int APIENTRY WinMain( HINSTANCE, // hInstance
                       HINSTANCE, // hPrevInstance
                       LPSTR, // lpCmdLine
@@ -13,20 +39,24 @@ int APIENTRY WinMain( HINSTANCE, // hInstance
 #else
 int main(int, const char**)
 #endif
-try
 {
-	srand((unsigned int) time(nullptr));
+	try
+	{
+		srand((unsigned int)time(nullptr));
 
 #if defined(_DEBUG) && defined(_WIN32) // memory leaks detection
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+		_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
-	
 
-	return 0;
-}
-catch (const std::exception &e)
-{
-	return 1;
+		//TODO::
+		auto path = CurrentWorkingDirectory();
+
+		return 0;
+	}
+	catch (const std::exception &e)
+	{
+		return 1;
+	}
 }
 
 
