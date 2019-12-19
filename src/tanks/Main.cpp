6a12566a1d7ecef867651eb006e18eb4c594b4b5
olitvinenko <alex.litvinenko.gm@gmail.com>
2019-12-19 11:38:53 +0200

    add glm library

--- src/tanks/Main.cpp
+++ src/tanks/Main.cpp
@@ -1,7 +1,7 @@
 #include "app/tzod.h"
 #include "app/View.h"
 #include "as/AppConstants.h"
-#include "FileSystem/FileSystem.h"
+#include "filesystem/FileSystem.h"
 #include "platglfw/GlfwAppWindow.h"
 #include "platglfw/Timer.h"
 #include "ui/ConsoleBuffer.h"
@@ -53,6 +53,22 @@ static UI::ConsoleBuffer s_logger(100, 500);
 //static long xxx = _CrtSetBreakAlloc(12649);
 
 #include "audio/SoundEngine.hpp"
+
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+
+glm::mat4 camera(float Translate, const glm::vec2& Rotate)
+{
+    glm::mat4 Projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
+    glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+    View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+    View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+    return Projection * View * Model;
+}
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
